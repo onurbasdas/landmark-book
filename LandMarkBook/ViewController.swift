80f449a6eab190fe723ed820a5f1171daba69fe4
onurbasdas <onurebasdase@hotmail.com>
2021-01-24 16:34:25 +0300

    show data created

--- LandMarkBook/ViewController.swift
+++ LandMarkBook/ViewController.swift
@@ -7,13 +7,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var landmarkNames = [String]()
+    var landmarkImages = [UIImage]()
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        //        Landmark Book Data
+        
+        landmarkNames.append("Collesium")
+        landmarkNames.append("Great Wall")
+        landmarkNames.append("Kremlin")
+        landmarkNames.append("Stonehenge")
+        landmarkNames.append("Taj Mahal")
+        
+        landmarkImages.append(UIImage(named: "collesium")!)
+        landmarkImages.append(UIImage(named: "greatwall")!)
+        landmarkImages.append(UIImage(named: "kremlin")!)
+        landmarkImages.append(UIImage(named: "stonehenge")!)
+        landmarkImages.append(UIImage(named: "tacmahal")!)
+        
+        
+        
     }
-
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = landmarkNames[indexPath.row]
+        return cell
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return landmarkNames.count
+    }
+    
+    
 }
 
